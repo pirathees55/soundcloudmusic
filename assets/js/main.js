c0b0329299d644ce4a03879b9c6c66b70bcadc9c
pirathees thevanthiram <piratheesthevanthiram@Piratheeskarans-MacBook-Pro.local>
2021-02-28 01:21:42 +0530

    soundcloud player code

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,7 @@ SC.initialize({
 });
 
 $(document).ready(function () {
-  SC.stream('/tracks/324547811', function (sound) {
+  SC.stream('/tracks/226001399', function (sound) {
       //function for start button
       // $('#a').mouseenter(function (e) {
       //     e.preventDefault();
@@ -60,9 +60,40 @@ $(document).ready(function () {
         sound.stop();
     });
 
+    SC.stream('/tracks/603749214', function (sound) {
+      $('.o').mouseenter(function (e) {
+          e.preventDefault();
+          sound.start();
+      });
+      $('.o').mouseleave(function (e) {
+          e.preventDefault();
+          sound.pause();
+      });
+      //function for start button
+  
+      $('#start2').click(function (e) {
+          e.preventDefault();
+          sound.start();
+      });
+  
+      //function for stop button
+  
+      $('#stop2').click(function (e) {
+          e.preventDefault();
+          sound.stop();
+      });
 
+
+    
 });
 
+
+
+
+
+
+
+//fina
 }); 
 
 
